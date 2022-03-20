42da4a5039ee8e3c790af7f9f64438b9824a5a58
smart-think-app <79594145+smart-think-app@users.noreply.github.com>
2022-03-20 15:22:48 +0700

    Update main.go

--- main.go
+++ main.go
@@ -23,5 +23,5 @@ func main() {
 
 // Handler
 func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World! version 2.5")
+	return c.String(http.StatusOK, "Hello, World! version 3.0")
 }
